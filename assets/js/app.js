92a3de183ebec5dd95f4bb182fbda96b51083286
Arifulislam03 <75440947+Arifulislam03@users.noreply.github.com>
2022-03-30 17:53:49 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+(function ($) {
+    "use strict";
+
+
+ /*
+    |====================
+    | Mobile NAv trigger
+    |=====================
+    */
+    
+    var trigger = $('.navbar-toggler'),
+      overlay     = $('.overlay'),
+      navc     = $('.navbar-collapse'),
+      active      = false;
+  
+
+      $('.navbar-toggler, .navbar-nav li a, .overlay').on('click', function () {
+          $('.navbar-toggler').toggleClass('active')
+        //   $('#js-navbar-menu').toggleClass('active');
+        //   $('.navbar-collapse').toggleClass('show');
+          overlay.toggleClass('active');
+          navc.toggleClass('active');
+      });  
+      
+         /*
+    | ==========================
+    | NAV FIXED ON SCROLL
+    | ==========================
+    */
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $(".nav-scroll").addClass("nav-strict");
+        } else {
+            $(".nav-scroll").removeClass("nav-strict");
+        }
+    });
+    
+
+}(jQuery));
